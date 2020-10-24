38c2fcb497a6a1118f963fe03631a15b4f8323de
Matt <ihate2020scam@gmail.com>
2020-10-24 09:53:44 +0100

    Products model, creatin new item on the website in django administration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+    icon = models.ImageField(upload_to = 'images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.blog_content[:100]
+
+    def publication_date_notime(self):
+        return self.publication_date.strftime('%e %b %Y')
+        # skraca czas tylko do daty, posortowane przy uzyciu srtftime (https://strftime.org/)
